0396f026c28df6eb341e9950045ad2c5e8f43d8e
Fernando Gurgel <fag.gurgel@gmail.com>
2020-06-19 01:17:21 -0400

    Ajuste do docker com todos sistemas

--- backend/src/repositories/CarsRepository.ts
+++
@@ -1,6 +0,0 @@
-// import { EntityRepository, Repository } from 'typeorm'
-// import Car from '../models/Car'
-
-// class CarRepository extends Repository<Car> {}
-
-// export default CarRepository
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API:"http://localhost:3333"
+  API:"http://172.19.0.2:3333"
 };
